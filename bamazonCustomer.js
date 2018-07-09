77dcdaaf133730c19cd2c8aa84b29da7aa34df19
aeffiong <aeffiong9@gmail.com>
2018-07-08 21:32:03 -0600

    connected to database and first set of inquirer questions working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,5 +17,29 @@ var connection = mysql.createConnection ({
 connection.connect(function(err) {
     if(err) throw err;
     console.log("connection successful!");
-    console.log("Welcome to bamazon! Select an item and how many you would like to purchase.");
+    console.log("Welcome to bamazon! Select an item by [item_id] that you would like to purchase.");
+    startPurchase();
 });
+
+function startPurchase() {
+    // query database for products
+    connection.query(
+        "SELECT item_id, product_name, department_name, price FROM products",
+        function(err, results) {
+            console.log(results);
+            // user selects which item id they want to purchase
+            inquirer
+            .prompt([{
+                name: "id",
+                type: "input",
+                message: "Enter the item id for the product you want to purchase."
+            },
+            {
+                name: "amount",
+                type: "input",
+                message: "How many do you want to buy?"
+            }
+        ]);
+        }
+    )
+}
