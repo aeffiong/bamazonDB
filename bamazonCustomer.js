9e9f71991f7a5063d5f6d8a057958f7f01941c26
aeffiong <aeffiong9@gmail.com>
2018-07-09 21:51:47 -0600

    able to pull from DB to update order and total price.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,15 +24,27 @@ connection.connect(function(err) {
 function startPurchase() {
     // query database for products
     connection.query(
-        "SELECT item_id, product_name, department_name, price FROM products",
+        "SELECT * FROM products",
         function(err, results) {
-            console.log(results);
+            for(var i = 0; i < results.length; i ++) {
+                console.log("---------------------------------------------------------------------------------------------\nItem ID: " 
+                + results[i].item_id + " | " + "Product: " + results[i].product_name 
+                + " | " + "Department: " + results[i].department_name + " | " + "Price: " + results[i].price +
+                " | " + "Quantity: " + results[i].stock_quantity);
+            }
+            
             // user selects which item id they want to purchase
             inquirer
             .prompt([{
                 name: "id",
                 type: "input",
-                message: "Enter the item id for the product you want to purchase."
+                message: "Enter the item id for the product you want to purchase.",
+                validate: function(value) {
+                    if(isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             },
             {
                 name: "amount",
@@ -46,15 +58,31 @@ function startPurchase() {
                 }
             }
         ]).then(function(answer) {
-            // check database to see if there is enough product to sell
-            var chosenProduct;
-            for(var i = 0; i < results.length; i++) {
-                chosenProduct = results[i];
-                console.log(chosenProduct);
-            }
-
             // if store has enough product, full order and update quantity in database and show customer total cost
-            // if(chosenProduct)
+            connection.query(
+                "SELECT * FROM products WHERE ? ",
+                {item_id: answer.id},
+                function(err, res) {
+                    if(err) throw err;
+                    console.log(res);
+                    console.log(answer.id);
+                    console.log(res[0].stock_quantity);
+                    if(answer.amount <= res[0].stock_quantity) {
+                        console.log("Success! Your item is in stock and your order is being placed!");
+                        console.log("Your total is: $ " + parseInt(answer.amount)*res[0].price);
+                    } else {
+                        console.log("Insuffient stock, please try again");
+                    }
+                }
+            )
+            // var chosenItem = [];
+            // for(var i = 0; i < results.length; i++) {
+                // console.log(chosenItem.push(results[i].stock_quantity))
+                // console.log("Item ID: " + answer.id + " " + "Quantity to purchase: " + answer.amount);
+            // }
+
+            
+            
 
             // if not enough quantity, say "insufficient quantity" and prevent order from going through
         })
