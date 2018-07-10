1622e694e465b175fb3e50268b3994957f071050
aeffiong <aeffiong9@gmail.com>
2018-07-09 22:19:20 -0600

    trying to update database after purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var inquirer = require("inquirer");
 var password = require("./config");
 
 // connecting to bamazon database
-var connection = mysql.createConnection ({
+var connection = mysql.createConnection({
     host: "localhost",
     port: 3307,
     user: "root",
@@ -14,33 +14,38 @@ var connection = mysql.createConnection ({
     database: "bamazon"
 });
 
-connection.connect(function(err) {
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connection successful!");
     console.log("Welcome to bamazon! Select an item by [item_id] that you would like to purchase.");
-    startPurchase();
+    displayStore();
 });
 
-function startPurchase() {
+function displayStore() {
     // query database for products
     connection.query(
         "SELECT * FROM products",
-        function(err, results) {
-            for(var i = 0; i < results.length; i ++) {
-                console.log("---------------------------------------------------------------------------------------------\nItem ID: " 
-                + results[i].item_id + " | " + "Product: " + results[i].product_name 
-                + " | " + "Department: " + results[i].department_name + " | " + "Price: " + results[i].price +
-                " | " + "Quantity: " + results[i].stock_quantity);
+        function (err, results) {
+            for (var i = 0; i < results.length; i++) {
+                console.log("---------------------------------------------------------------------------------------------\nItem ID: "
+                    + results[i].item_id + " | " + "Product: " + results[i].product_name
+                    + " | " + "Department: " + results[i].department_name + " | " + "Price: $" + results[i].price +
+                    " | " + "Quantity: " + results[i].stock_quantity);
             }
-            
-            // user selects which item id they want to purchase
-            inquirer
+            startPurchase();
+
+
+        });
+
+    function startPurchase() {
+        // user selects which item id they want to purchase
+        inquirer
             .prompt([{
                 name: "id",
                 type: "input",
                 message: "Enter the item id for the product you want to purchase.",
-                validate: function(value) {
-                    if(isNaN(value) === false) {
+                validate: function (value) {
+                    if (isNaN(value) === false) {
                         return true;
                     }
                     return false;
@@ -50,42 +55,57 @@ function startPurchase() {
                 name: "amount",
                 type: "input",
                 message: "How many do you want to buy?",
-                validate: function(value) {
-                    if(isNaN(value) === false) {
+                validate: function (value) {
+                    if (isNaN(value) === false) {
                         return true;
                     }
                     return false;
                 }
             }
-        ]).then(function(answer) {
-            // if store has enough product, full order and update quantity in database and show customer total cost
-            connection.query(
-                "SELECT * FROM products WHERE ? ",
-                {item_id: answer.id},
-                function(err, res) {
-                    if(err) throw err;
-                    console.log(res);
-                    console.log(answer.id);
-                    console.log(res[0].stock_quantity);
-                    if(answer.amount <= res[0].stock_quantity) {
-                        console.log("Success! Your item is in stock and your order is being placed!");
-                        console.log("Your total is: $ " + parseInt(answer.amount)*res[0].price);
-                    } else {
-                        console.log("Insuffient stock, please try again");
+            ]).then(function (answer) {
+                // if store has enough product, full order and update quantity in database and show customer total cost
+                connection.query(
+                    "SELECT * FROM products WHERE ? ",
+                    { item_id: answer.id },
+                    function (err, res) {
+                        if (err) throw err;
+                        // console.log(res);
+                        // console.log(answer.id);
+                        // console.log(res[0].stock_quantity);
+                        if (answer.amount <= res[0].stock_quantity) {
+                            console.log("Success! Your item is in stock and your order is being placed!");
+                            console.log("Your total is: $" + parseInt(answer.amount) * res[0].price);
+                            // update store quantity - this is where I'm stuck: new function?
+                            connection.query(
+                                "UPDATE products SET stock_quantity WHERE ? ",
+                                {
+                                    item_id: answer.id
+                                },
+                                function (error, result) {
+                                    if (error) throw error;
+                                    console.log(result);
+
+                                }
+                            )
+                        } else {
+                            console.log("Insuffient stock, please try again");
+                            displayStore();
+                        }
                     }
-                }
-            )
-            // var chosenItem = [];
-            // for(var i = 0; i < results.length; i++) {
+                )
+                // var chosenItem = [];
+                // for(var i = 0; i < results.length; i++) {
                 // console.log(chosenItem.push(results[i].stock_quantity))
                 // console.log("Item ID: " + answer.id + " " + "Quantity to purchase: " + answer.amount);
-            // }
+                // }
 
-            
-            
 
-            // if not enough quantity, say "insufficient quantity" and prevent order from going through
-        })
-        }
-    )
+
+
+                // if not enough quantity, say "insufficient quantity" and prevent order from going through
+            })
+    }
+
 }
+
+
