33bb17ff96d1b236030de7219e6f7971095ffd93
aeffiong <aeffiong9@gmail.com>
2018-07-13 22:16:56 -0600

    final

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 ## Bamazon
 
+Bamazon is a CLI where the user can shop for certain items from a MySQL database. Bamazon shows how to pull data from a database and update it based on user input. 
+
+The interface shows the user a list of items, their id, description, and price. The user then selects an item to purchase by the item id. After the user selects an item, he/she is asked how many to purchase. 
+
+If there is enough in stock, the purchase will go through, the user will be told their total cost, and the database will update the amount of product in stock. 
+
+If there is not enough of the item in stock, the user will be notified that there is not enough product. The purchase will not go through, and the user will be able to continue shopping. 
+
 ![bamazon](https://github.com/aeffiong/bamazonDB/blob/master/bamazon.gif)
+
+### Technologies Used:
+
+* Node
+* MySQL
+* JavaScript
+* mysql npm package
+* inquirer npm package
+
+### Built with: 
+
+* MySQL Workbench
+* Visual Studio Code
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,113 +1,3 @@
-// // requiring mysql and inquirer npm packages
-// var mysql = require("mysql");
-// var inquirer = require("inquirer");
-
-// // mysql password details
-// var password = require("./config");
-
-// // connecting to bamazon database
-// var connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3307,
-//     user: "root",
-//     password: password.password,
-//     database: "bamazon"
-// });
-
-// connection.connect(function (err) {
-//     if (err) throw err;
-//     console.log("connection successful!");
-//     console.log("Welcome to bamazon! Select an item by [item_id] that you would like to purchase.");
-//     displayStore();
-// });
-
-// function displayStore() {
-//     // query database for products
-//     connection.query(
-//         "SELECT * FROM products",
-//         function (err, results) {
-//             for (var i = 0; i < results.length; i++) {
-//                 console.log("---------------------------------------------------------------------------------------------\nItem ID: "
-//                     + results[i].item_id + " | " + "Product: " + results[i].product_name
-//                     + " | " + "Department: " + results[i].department_name + " | " + "Price: $" + results[i].price.toFixed(2) +
-//                     " | " + "Quantity: " + results[i].stock_quantity);
-//             }
-//             startPurchase();
-
-
-//         });
-
-//     function startPurchase() {
-//         // user selects which item id they want to purchase
-//         inquirer
-//             .prompt([{
-//                 name: "id",
-//                 type: "input",
-//                 message: "Enter the item id for the product you want to purchase.",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             },
-//             {
-//                 name: "amount",
-//                 type: "input",
-//                 message: "How many do you want to buy?",
-//                 validate: function (value) {
-//                     if (isNaN(value) === false) {
-//                         return true;
-//                     }
-//                     return false;
-//                 }
-//             }
-//             ]).then(function (answer) {
-//                 // if store has enough product, full order and update quantity in database and show customer total cost
-//                 connection.query(
-//                     "SELECT * FROM products WHERE ? ",
-//                     { item_id: answer.id },
-//                     function (err, res) {
-//                         if (err) throw err;
-//                         // console.log(res);
-//                         // console.log(answer.id);
-//                         // console.log(res[0].stock_quantity);
-
-//                         if (answer.amount <= res[0].stock_quantity) {
-//                             console.log("Success! Your item is in stock and your order is being placed!");
-//                             console.log("Your total is: $" + parseInt(answer.amount) * res[0].price.toFixed(2));
-
-//                         } else {
-//                             console.log("Insuffient stock, please try again");
-//                             displayStore();
-//                         }
-//                     }
-//                 )
-
-//                 // update store quantity - this is where I'm stuck: new function?
-//                 // connection.query(
-//                 //     "UPDATE products SET stock_quantity WHERE ? ",
-//                 //     {item_id: answer.id},
-//                 //     function (error, result) {
-//                 //         if (error) throw error;
-//                 //         console.log(result);
-
-//                 //     }
-//                 // )
-//                 // var chosenItem = [];
-//                 // for(var i = 0; i < results.length; i++) {
-//                 // console.log(chosenItem.push(results[i].stock_quantity))
-//                 // console.log("Item ID: " + answer.id + " " + "Quantity to purchase: " + answer.amount);
-//                 // }
-
-
-
-
-//                 // if not enough quantity, say "insufficient quantity" and prevent order from going through
-//             })
-//     }
-
-// }
 
 // requiring mysql and inquirer npm packages
 var mysql = require("mysql");
@@ -167,21 +57,14 @@ function displayStore() {
                     }
                 }
                 ]).then(function (answer) {
-                    // if store has enough product, full order and update quantity in database and show customer total cost
-                    // console.log(results);
-                    // var chosenItem;
-                   
                     connection.query(
                         "SELECT * FROM products WHERE ? ",
                         { item_id: answer.id },
                         function (err, res) {
                             if (err) throw err;
                             for (var i = 0; i < res.length; i++) {
-                                // chosenItem = results[i];
+
                             }
-                            // console.log(res);
-                            // console.log(answer.id);
-                            // console.log(res[0].stock_quantity);
 
                             if (answer.amount <= res[0].stock_quantity) {
                                 console.log("Success! Your item is in stock and your order is being placed!");
@@ -197,16 +80,16 @@ function displayStore() {
                                         if (err) throw err;
                                         console.log("Your order has been completed! Thanks!");
                                         connection.end();
-                                        
+
                                     }
-                        
+
                                 )
 
                             } else {
                                 console.log("Insuffient stock, please try again");
                                 displayStore();
                             }
-                            
+
                         }
                     )
 
